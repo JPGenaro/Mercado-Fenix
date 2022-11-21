cd3085ea80e9160c77686356c11621147c3ac681
JFIEROLI <110985726+JFIEROLI@users.noreply.github.com>
2022-11-21 18:10:15 -0300

    Implementacion de usuarios y otros

---
+++ clases.py
@@ -0,0 +1,28 @@
+from enum import nonmember
+
+
+class Usuario:
+    def __init__(self, nombre, contra):
+        self.nombre = nombre
+        self.contra = contra
+        self.dinero = 0
+
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+#Productos
+prod_1 = Producto(
+    "Galletas",
+    20
+)
+prod_2 = Producto(
+    "Jugos",
+    30
+)
+prod_3 = Producto(
+    "Gaseosas",
+    50
+)
+productos = [prod_1, prod_2, prod_3]
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #Importaciones
 import sys
 
-from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2.QtWidgets import QMainWindow, QApplication, QMessageBox
 
 # Ventanas y archivos
 from ventanas.ui_Identificacion import Ui_Identificador
 from ventanas.home_ui import Ui_home
+from clases import Usuario, Producto, productos
+
+#Variables y listas
+prueba = Usuario(
+    "s",
+    "s"
+)
+usuarios = [prueba]
 
 #Codigo
 class MainWindow(QMainWindow):
@@ -20,18 +28,45 @@ class MainWindow(QMainWindow):
 
         #Botones
         self.ui.B_inicio_sesion.clicked.connect(self.w_home)
-        self.ui.B_registro.connect(self.w_home)
+        self.ui.B_registro.clicked.connect(self.registrar)
 
-
-    #Ventana del home
     def w_home(self):
         self.ui = Ui_home()
         self.ui.setupUi(self)
 
         #Botones
-        # self.ui.B_compras.clicked.connect()
+        #self.ui.B_compras.clicked.connect()
+        #self.ui.B_tienda.clicked.connect()
         self.ui.B_salir.clicked.connect(self.w_sesion)
-        # self.ui.B_tienda.clicked.connect()
+
+
+    #Registrar usuarios
+    def registrar(self):
+        mensaje = QMessageBox(self)
+        mensaje.setWindowTitle('Mensaje')
+        registro_nombre = self.ui.txt_registro_usuario.text().strip()
+        registro_contra = self.ui.txt_registro_contra.text().strip()
+        passed = False
+        for i in range(len(usuarios)):
+            if not usuarios[i].nombre == registro_nombre:
+                passed = True
+            else:
+                passed = False
+                mensaje.setIcon(QMessageBox.Warning)
+                mensaje.setText('El nombre deseado ya ha sido elegido por otro usuario')
+        if passed == True:
+            molde = Usuario(
+                registro_nombre,
+                registro_contra
+            )
+            usuarios.append(molde)
+            mensaje.setIcon(QMessageBox.Information)
+            mensaje.setText('Usuario registrado correctamente')
+        mensaje.exec_()
+
+    #Iniciar usuarios
+    def iniciar_sesion(self):
+        pass
 
 
 
